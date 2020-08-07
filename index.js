1b666bf68bb6fc97c2d6a9c94908c073771517e4
Nick Reisenauer <nickdreisenauer@gmail.com>
2020-08-06 20:13:51 -0700

    Added custom image widths

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const image = document.querySelector("#image");
 const button = document.querySelector("#button");
 
 button.addEventListener("click", () => {
-  image.src = `https://source.unsplash.com/random/400x600?sig=${
+  image.src = `https://source.unsplash.com/random/350x450?sig=${
     Math.random() * 9999
   }`;
   console.log(image);
